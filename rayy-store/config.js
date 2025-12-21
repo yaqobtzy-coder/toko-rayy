9b6149e01b8b7377e3ad779025e230f0f5a33862
yaqobtzy-coder <yaqobtzy@gmail.com>
2025-12-21 10:39:02 +0700

    Update config.js

--- rayy-store/config.js
+++ rayy-store/config.js
@@ -1,13 +1,7 @@
-// PUSAT PENGATURAN SISTEM
 const CONFIG = {
-    // Akun Login Dev Utama
     devUsername: "rayy_dev",
     devPassword: "085794",
-    
-    // Akun Owner (Untuk Proteksi)
     ownerUsername: "admin_rayy",
-    
-    // Pesan Sistem
     pesanBlokirDev: "Username ini adalah username Dev utama untuk semua layanan di semua web nya!",
     pesanBukanDev: "Akun/username ini tidak seharusnya di web ini!"
 };
