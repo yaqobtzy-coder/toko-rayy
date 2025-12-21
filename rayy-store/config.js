70d3d68d086fceacb4d4ef7a5aefff24c4f7747e
yaqobtzy-coder <yaqobtzy@gmail.com>
2025-12-21 10:24:25 +0700

    Update config.js

--- rayy-store/config.js
+++ rayy-store/config.js
@@ -1,8 +1,17 @@
-// File ini hanya disimpan oleh Rayy / Developer
-const USERS_DATABASE = [
-    { user: "admin_rayy", pass: "085794", name: "Rayy Utama" },
-    { user: "kasir1", pass: "123123", name: "Kasir Depan" },
-    { user: "staf_rayy", pass: "556677", name: "Staf Gudang" }
-];
-
-const UNLOCK_KEY = "buka-akses-rayy";
+const CONFIG = {
+    // Kredensial Developer Utama
+    devUsername: "rayy_dev",
+    devPassword: "085794",
+    
+    // Kredensial Owner
+    ownerUsername: "admin_rayy",
+    ownerPassword: "rayy123",
+    
+    // Informasi Toko & Kontak
+    waNumber: "628131172093",
+    namaToko: "Rayy Store",
+    
+    // Pesan Sistem (Sesuai Permintaanmu)
+    pesanBlokirDev: "Username ini adalah username Dev utama untuk semua layanan di semua web nya!",
+    pesanBukanDev: "Akun/username ini tidak seharusnya di web ini!"
+};
