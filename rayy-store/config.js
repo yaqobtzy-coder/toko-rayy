93dda9d50ad457cfbc6d5e144630fd2494cd0c08
yaqobtzy-coder <yaqobtzy@gmail.com>
2025-12-21 07:38:12 +0700

    Update sistem toko rayy terbaru

---
+++ rayy-store/config.js
@@ -0,0 +1,8 @@
+// File ini hanya disimpan oleh Rayy / Developer
+const USERS_DATABASE = [
+    { user: "admin_rayy", pass: "085794", name: "Rayy Utama" },
+    { user: "kasir1", pass: "123123", name: "Kasir Depan" },
+    { user: "staf_rayy", pass: "556677", name: "Staf Gudang" }
+];
+
+const UNLOCK_KEY = "buka-akses-rayy";
